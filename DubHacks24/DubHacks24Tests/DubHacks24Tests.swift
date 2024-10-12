6d9744d49939ad75661e9ae716c51166e9493fd4
Alex Knotwell <aknot@Alexs-MacBook-Air-6.local>
2024-10-12 12:58:38 -0700

    Added XCode Project Files

---
+++ DubHacks24/DubHacks24Tests/DubHacks24Tests.swift
@@ -0,0 +1,17 @@
+//
+//  DubHacks24Tests.swift
+//  DubHacks24Tests
+//
+//  Created by Alex Knotwell on 10/12/24.
+//
+
+import Testing
+@testable import DubHacks24
+
+struct DubHacks24Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
