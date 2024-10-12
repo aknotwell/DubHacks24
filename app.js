a1895ce0b0403c5566065da62abb795143e68375
Alex Knotwell <aknot@Alexs-MacBook-Air-6.local>
2024-10-12 13:37:04 -0700

    Created package.json file, app.js to run node/backend

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+    res.send("hello world");
+});
+const port = 3000;
+
+app.listen(port, () => {
+console.log("Server is running on port: " + port);
+});
